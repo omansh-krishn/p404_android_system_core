961209d5fab4508121974f967e4f1fbd6de338a0
Michael Bestas <mkbestas@lineageos.org>
2018-03-24 21:31:02 +0200

    init: Don't run update_sys_usb_config if /data isn't mounted
    * When /data is not mounted / not yet decrypted,
      persistent property overrides are not loaded yet
      so persist.sys.usb.config value is the one from default.prop.
    
      This caused adb to always be disabled on boot even if
      it was enabled in development settings.
    
    Change-Id: Ic2a51591ad6b77f3463cfa91e9d7362410a021f2
    Signed-off-by: Omansh Krishn <omansh@duck.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1022,7 +1022,9 @@ void PropertyLoadBootDefaults() {
     property_initialize_ro_product_props();
     property_derive_build_props();
 
-    update_sys_usb_config();
+    if (android::base::GetBoolProperty("ro.persistent_properties.ready", false)) {
+        update_sys_usb_config();
+    }
 }
 
 bool LoadPropertyInfoFromFile(const std::string& filename,
