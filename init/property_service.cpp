8add27d21097cffa78176dc324b67f0ff8d8e5e2
Jarl-Penguin <jarlpenguin@outlook.com>
2021-06-16 11:28:46 +0000

    init: Don't spoof SafetyNet properties in recovery mode
    Change-Id: Ib6d3808c3b8f3e0cffab685a24d3cdd436b0fe9b
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1434,7 +1434,9 @@ void PropertyInit() {
     }
 
     // Report valid verified boot chain to help pass Google SafetyNet integrity checks
-    SetSafetyNetProps();
+    if (!IsRecoveryMode()) {
+        SetSafetyNetProps();
+    }
 
     // If arguments are passed both on the command line and in DT,
     // properties set in DT always have priority over the command-line ones.
